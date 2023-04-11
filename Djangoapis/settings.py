7ca51e02a056f04a35bc3d5a43147f64e87f7841
ritual-ai <admin@ritual-ai.net>
2023-04-11 19:03:57 +0200

    updating urls for vm

--- Djangoapis/settings.py
+++ Djangoapis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-viz)=zj9-35v2(!-c9^0hj#uf_vhc#@r#b=^@12h2lw(j+ykh(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['130.60.156.180', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -121,8 +121,7 @@ USE_TZ = True
 
 CORS_ALLOWED_ORIGINS = [
    "http://localhost:3000",
-   "http://mayportfolio.com",
-   "http://localhost:4200",
+   "http://130.60.156.180:4200",
    "http://192.168.0.102:4200"
 ]
 

f4458fb025fec4a3ffa13b636b75964394520fc1
ritual-ai <admin@ritual-ai.net>
2023-04-11 18:28:29 +0200

    test2

--- apis/admin.py
+++ apis/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import CustomUser, Scenario, ScenarioSolution
 # Register your models here.
-
+#useless comment
 admin.site.register(CustomUser)
 admin.site.register(Scenario)
 admin.site.register(ScenarioSolution)
